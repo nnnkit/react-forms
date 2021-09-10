001941a4fb426128d40aa59e54e9bac4e7779b53
Ankit <iankit17@gmail.com>
2021-09-10 14:25:35 +0530

    Updates

---
+++ precache-manifest.0b964a6c814e0926e9848488c60ba5f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba34768d942c8ee79cec86c963727b4",
+    "url": "/react-forms/index.html"
+  },
+  {
+    "revision": "a82245b3eba699cae234",
+    "url": "/react-forms/static/css/2.47443430.chunk.css"
+  },
+  {
+    "revision": "55671a235ae0ea31c873",
+    "url": "/react-forms/static/css/main.1f4153b5.chunk.css"
+  },
+  {
+    "revision": "a82245b3eba699cae234",
+    "url": "/react-forms/static/js/2.6bfa430e.chunk.js"
+  },
+  {
+    "revision": "55671a235ae0ea31c873",
+    "url": "/react-forms/static/js/main.41bdb6a3.chunk.js"
+  },
+  {
+    "revision": "ae477fb449b1a1b38199",
+    "url": "/react-forms/static/js/runtime~main.82238d43.js"
+  }
+]);
