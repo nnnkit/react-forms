f755ea993133b09057bbbd50067f502d658eef8f
Ankit <iankit17@gmail.com>
2021-09-10 13:43:14 +0530

    add controlled component

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => {
+  return (
+    <section className="hero is-dark has-text-centered">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title"> 📑 Learning About Forms</h1>
+          <h2 className="subtitle">Controlled Component</h2>
+        </div>
+      </div>
+    </section>
+  );
+};
