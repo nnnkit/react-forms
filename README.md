8fd4a2ff3718fc17ed6596420c6b7f06d36f49b2
Ankit <iankit17@gmail.com>
2021-09-10 14:28:31 +0530

    add emoji

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Forms
+
+This repo contains an app for shipping and billing address. It will help you practice taking input from user using different input elements.
--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ export default () => {
     <section className="hero is-dark has-text-centered">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title"> 📑 Learning About Forms</h1>
+          <h1 className="title"> 📑 Learning About Forms 📑</h1>
           <h2 className="subtitle">Controlled Component</h2>
         </div>
       </div>
